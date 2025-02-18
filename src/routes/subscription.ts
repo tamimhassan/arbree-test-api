a327e8d0bc5b97e501e210b6dd3629b60bc9bfb6
tamimhassan <tamimhassan506@gmail.com>
2025-02-18 18:15:15 +0600

    Update subscription route

--- src/routes/subscription.ts
+++ src/routes/subscription.ts
@@ -9,8 +9,8 @@ import {
 const router = express.Router();
 
 router.post("/", createSubscription);
-router.get("/", getUserSubscriptions);
-router.put("/", updateSubscription);
-router.delete("/", cancelSubscription);
+router.get("/:userId", getUserSubscriptions);
+router.put("/:subscriptionId", updateSubscription);
+router.delete("/:subscriptionId", cancelSubscription);
 
 export default router;
